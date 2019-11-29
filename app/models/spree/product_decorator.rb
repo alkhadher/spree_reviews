0688e92b45f8380ff2f31a166135eb95fe44bc04
Khadher Alqiari <k.alqiari@floranow.com>
2019-11-29 02:33:17 +0200

    Upgrade to spree 4

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,18 +1,23 @@
+# frozen_string_literal: true
+
 # Add access to reviews/ratings to the product model
-Spree::Product.class_eval do
-  has_many :reviews
 
-  def stars
-    avg_rating.try(:round) || 0
-  end
-
-  def recalculate_rating
-    self[:reviews_count] = reviews.reload.approved.count
-    if reviews_count > 0
-      self[:avg_rating] = reviews.approved.sum(:rating).to_f / reviews_count
-    else
-      self[:avg_rating] = 0
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.has_many :reviews
     end
-    save
+
+    def recalculate_rating
+      self[:reviews_count] = reviews.reload.approved.count
+      self[:avg_rating] = reviews_count.positive? ? (reviews.approved.sum(:rating).to_f / reviews_count) : 0
+      save
+    end
+
+    def stars
+      avg_rating.try(:round) || 0
+    end
+
   end
 end
+::Spree::Product.prepend Spree::ProductDecorator
